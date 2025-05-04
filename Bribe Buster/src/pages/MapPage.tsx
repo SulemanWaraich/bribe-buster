3048a9abe80ca93ed7ede0207a5fd3cd3eaa357d
Muhammad Suleman <tech4you330@gmail.com>
2025-05-04 21:05:44 +0500

    feat(router): add two pages with embedded components and integrate them into app routing

---
+++ Bribe Buster/src/pages/MapPage.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import Heatmap from '../components/HeatMap';
+import Leaderboard from '../components/Leaderboard';
+import { BribeReport } from '../types';
+
+export default function MapPage() {
+  const [reports, setReports] = useState<BribeReport[]>([]);
+
+  useEffect(() => {
+    const storedReports = JSON.parse(localStorage.getItem('bribeReports') || '[]');
+    setReports(storedReports);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-900 py-10 px-4 lg:px-10 ">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-10">
+        {/* Left: Heatmap */}
+        <div className="lg:col-span-2 space-y-6 ">
+          <div>
+            <h1 className="text-4xl font-extrabold text-white ">Corruption Heatmap</h1>
+            <p className="text-base text-white mt-1">
+              Explore geographic distribution of reported bribes.
+            </p>
+          </div>
+          <Heatmap />
+        </div>
+
+        {/* Right: Sidebar */}
+        <div className="space-y-6 ">
+          <h1 className='text-3xl text-white font-extrabold text-center'>Departments with Highest Reported Bribes</h1>
+
+          <Leaderboard reports={reports} />
+
+          <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-100">
+            <h3 className="text-lg font-semibold text-gray-800 mb-3">How to Use</h3>
+            <ul className="list-disc pl-5 text-sm text-gray-600 space-y-2">
+              <li><span className="text-red-500 font-bold">Red markers</span> = High bribe amounts</li>
+              <li>Click markers for details</li>
+              <li>Data updates automatically</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
